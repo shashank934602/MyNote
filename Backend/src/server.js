fc2f152c5da8fcdd14599d076eaf603a1fc2be71
shashank934602 <93077450+shashank934602@users.noreply.github.com>
2025-08-06 21:28:50 +0530

    prepared for the deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js"
 import connectDB from "./config/db.js";
@@ -12,16 +13,29 @@ import rateLimiter from "../middleware/rateLimiter.js";
 dotenv.config()
 const app  = express()
 const port = process.env.port||5001
+const __dirname = path.resolve()
 
-app.use(cors({
+if (process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin: "http://localhost:5173"
-}))
+})
+    )
+
+}
+
 app.use(express.json())
 app.use(rateLimiter)
 
 app.use("/api/notes",notesRoutes);
 
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static (path.join(_dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  }); I
+}
 
 connectDB().then(()=>{
 app.listen(port,()=>{
