85a5fc115cf807e3add6820180cfd22bf2b31533
shashank934602 <93077450+shashank934602@users.noreply.github.com>
2025-08-06 21:51:11 +0530

    prepared for the deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -31,7 +31,7 @@ app.use("/api/notes",notesRoutes);
 app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
 if (process.env.NODE_ENV === "production"){
-  app.use(express.static (path.join(_dirname, "../frontend/dist")));
+  app.use(express.static (path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   }); I
